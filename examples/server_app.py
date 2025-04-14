25b5ff3482984cf5389bede70416d90c03c53b92
Jonas Schroeter <jonas.schroeter@stud.tu-darmstadt.de>
2025-04-14 16:54:41 +0200

    server gracyfully shuts down on ctrl c

--- examples/server_app.py
+++ examples/server_app.py
@@ -7,22 +7,27 @@ from aioquic.quic.events import StreamDataReceived
 class EchoProtocol(QuicConnectionProtocol):
     def quic_event_received(self, event):
         if isinstance(event, StreamDataReceived):
-            print(f"[server] Received data: {event.data.decode()}")
+            print(f"[server] Received data: {len(event.data)}")
             # Echo the data back to the sender
-            self._quic.send_stream_data(event.stream_id, event.data, end_stream=True)
-            self.transmit()
+            #self._quic.send_stream_data(event.stream_id, event.data, end_stream=True)
+            #self.transmit()
 
 # Start the server and listen for connections
 async def main():
     config = QuicConfiguration(is_client=False, alpn_protocols=["hq-29"])
     config.load_cert_chain(certfile="../tests/ssl_cert.pem", keyfile="../tests/ssl_key.pem")
+    port = 4433
+    host = "localhost"
 
+    print(f"[server] Running on {host}:{port}")
     # Listen for incoming QUIC connections on port 4433
-    server = await serve("localhost", 4433, configuration=config, create_protocol=EchoProtocol)
+    server = await serve(host, port, configuration=config, create_protocol=EchoProtocol)
 
-    
     await asyncio.Future()
 
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("[server] Shutdown complete.")
