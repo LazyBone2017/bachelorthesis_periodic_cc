f541423d6a953d75dbebab3946a11e4ffc298d74
Jonas Schroeter <jonas.schroeter@stud.tu-darmstadt.de>
2025-09-20 16:52:36 +0200

    updates setup script

--- setup.sh
+++ setup.sh
@@ -12,4 +12,7 @@ fi
 # 2. activate and install deps
 source .venv/bin/activate
 pip install --upgrade pip
+pip install -e .
 pip install -r requirements.txt
+
+echo "Environment set up successfully. Execute scripts in /thesis/config to run application."
