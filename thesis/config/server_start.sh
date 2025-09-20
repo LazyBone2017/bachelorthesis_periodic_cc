9e7ed2dfc9f8999e9854a6222822e9881530aeca
Jonas Schroeter <jonas.schroeter@stud.tu-darmstadt.de>
2025-09-20 16:41:24 +0200

    adds start scripts

--- thesis/config/server_start.sh
+++ thesis/config/server_start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-sudo ip netns exec ns2 /home/jonas/aioquic/environment/bin/python ../server_app.py
+set -e
+SCRIPT_DIR="$(dirname "$(realpath "$0")")/.."
+ROOT_DIR="$(realpath "$SCRIPT_DIR/..")"
+
+sudo ip netns exec ns2 \
+    "$ROOT_DIR/.venv/bin/python" \
+    "$ROOT_DIR/thesis/server_app.py"
