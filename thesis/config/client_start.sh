79dceed3034f2718d7045b881b0446d1362fc842
Jonas Schroeter <jonas.schroeter@stud.tu-darmstadt.de>
2025-09-27 13:37:22 +0200

    adds script arguments to start

--- thesis/config/client_start.sh
+++ thesis/config/client_start.sh
@@ -3,6 +3,11 @@ set -e
 SCRIPT_DIR="$(dirname "$(realpath "$0")")/.."
 ROOT_DIR="$(realpath "$SCRIPT_DIR/..")"
 
+if [ -z "$1" ]; then
+    echo "Please provide a configuration file."
+    exit 1
+fi
+
 sudo ip netns exec ns1 \
     "$ROOT_DIR/.venv/bin/python" \
-    "$ROOT_DIR/thesis/client_session.py"
+    "$ROOT_DIR/thesis/client_session.py" --config "$1"
